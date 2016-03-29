bdbca413bd94558ff7ed4582dbfdc435565840d9
CJAllen056 <christopherjallen056@gmail.com>
2016-03-29 10:09:21 +0100

    Updated comments on javascript file

--- js/app.js
+++ js/app.js
@@ -1,26 +1,31 @@
-// Goes need to alternate between splitting vertically and horizontally.
-// When a box is clicked, it needs to be replaced with two boxes of half the size. The direction of halving will alternate from turn to turn.
-// The number of moves needs to count up
-// When four or more boxes of the same size and shape line up in a grid of at least 2x2, they are taken out of play and given a timer. The timer counts down every time a move is made. When the timer on a box reaches zero the box dissapears and all boxes above fall into the space
-// There is a score which goes up every time a box ticks down
+////// Goes need to alternate between splitting vertically and horizontally.
+////// When a box is clicked, it needs to be replaced with two boxes of half the size. The direction of halving will alternate from turn to turn.
+////// The number of moves needs to count up
+////// When four or more boxes of the same size and shape line up in a grid of at least 2x2, they are taken out of play and given a timer. The timer counts down every time a move is made. When the timer on a box reaches zero the box dissapears and all boxes above fall into the space
+////// There is a score which goes up every time a box ticks down
 
-// need a function that alternates between horizontal and vertical split
-// need a function that counts up for every click on the screen
-// need to detect when 4 boxes of the same shape are in the right configuration
+////// need a function that alternates between horizontal and vertical split
+////// need a function that counts up for every click on the screen
+////// need to detect when 4 boxes of the same shape are in the right configuration
 
-//when a div is clicked, take the height and width of the div. divide it by two and replace it with a div that is half the size in the necessary direction
+////// when a div is clicked, take the height and width of the div. divide it by two and replace it with a div that is half the size in the necessary direction
+
+
+// The whole game is stored in the object: split.
 
 var split = split || {}
 
-// Turn determines how many turns have passed since the beginning of the game
-// Direction is used to determine which direction the boxes will split when clicked
+// Turn determines how many turns have passed since the beginning of the game.
+// Score determines the players score.
+// Direction is used to determine which direction the boxes will split when clicked.
+// DivId is used to give each new div that is created a unique id.
 
 split.turn = 0;
 split.score = 0;
 split.direction = "horiz";
 split.divId = 0;
 
-// The play function runs when the dom loads, and applies click events to all divs in the #playArea section, including any div events that are generated after the dom loads.
+// The play function runs when the dom loads, and is used to set the event listeners on all of the buttons in the game, which includes the divs in the play area, the colour pickers and the restart button on the game over screen.
 
 split.play = function() {
   $("#playArea").on("click", "div", split.setListeners);
@@ -28,13 +33,13 @@ split.play = function() {
   split.colourListeners();
 }
 
-// The setListeners function is the function that play uses to set the listeners....
+// The setListeners function is what is used to control the gameplay. It is activated whenever one of the divs in the play area is clicked, taking a number of variables about the clicked div and then running functions to split the div, check for matches or a loss, and update the display. The function also contains an if statement to prevent players clicking on boxes which they should not be able to.
 
 split.setListeners = function() {
   var divClass = $(this).attr("class");
   var divWidth = $(this).attr("class").substr(1, 2);
   var divHeight = $(this).attr("class").substr(4);
-  
+
   if (split.direction === "horiz" && (divHeight <= 1 || divClass.indexOf("matchedDiv") !== -1)) {
     return;
   } else if (split.direction === "vert" && (divWidth <= 1 || divClass.indexOf("matchedDiv") !== -1)) {
@@ -62,7 +67,7 @@ split.setListeners = function() {
   split.displayScore();
 }
 
-// The splitBox function splits any clicked box in half, replacing it with two boxes of half the size. The direction of the split is determined by the variable: direction. If the box is too small, it cannot be split. It positions the new boxes absolutely, based on the previous boxes position.
+// The splitBox function splits any clicked box in half, replacing it with two boxes of half the size. The direction of the split is determined by the variable: direction. If the box is too small, the split is prevented by the play function (above). The position of the two new boxes is determined by the position of the original boxes, and all boxes created have absolute positioning within the play area.
 
 split.splitBox = function(width, height, widthpx, heightpx, top, left) {
   if (split.direction === "horiz") {
@@ -74,7 +79,7 @@ split.splitBox = function(width, height, widthpx, heightpx, top, left) {
   }
 }
 
-// The checkMatches function checks the boxes surrounding the clicked box to see if there is a group of 4 boxes of the same size and shape.
+// Whenever a split is made, the checkMatches function checks the board to see if any matches have been made (a match is made when four equaly sized and shaped boxes are together in a box). It takes the size of the new divs created by the splitBox function and checks if there are any similar boxes in positions on the board which would create a rectangle, using the absolute positioning of the boxes created.
 
 split.checkMatches = function(width, height, top, left, classX) {
   if (split.direction === "horiz") {
@@ -134,7 +139,7 @@ split.checkMatches = function(width, height, top, left, classX) {
   }
 }
 
-// The setToMatched function gives the matched boxes found in the checkMatches function the correct properties for matched boxes.
+// Once a match has been detected by the checkMatches function, the setToMatched function will give the matched boxes the correct properties. This involves setting their class to "matchedDiv" (giving the box appropriate styling from the css), and giving the box a number based on the turn on which it was matched.
 
 split.setToMatched = function(element, id, height) {
   var div1 = $("#" + (split.divId - 2));
@@ -158,7 +163,7 @@ split.setToMatched = function(element, id, height) {
   div4.css("line-height", height/2 + "px");
 }
 
-// The matchedCountdown function makes the html values in matched (greyed out) divs count down by 1 each turn
+// The matchedCountdown counts down the numbers in the matched divs by one every turn, and increases the player score by one for every tick on these divs.
 
 split.matchedCountdown = function() {
   $(".matchedDiv").each(function() {
@@ -167,6 +172,8 @@ split.matchedCountdown = function() {
     split.score++;
   });
 }
+
+// The replaceMatchedAtZero function checks the board every turn to see if any of the matched divs have ticked down to zero. Once a grid of four boxes reaches zero it will be replaced by a larger div which is the size of the whole grid of four. The function also adds to the player score when the boxes are replaced. The amount added depends on the size of the replaced boxes.
 
 split.replaceMatchedAtZero = function() {
   $(".matchedDiv").each(function() {
@@ -195,7 +202,7 @@ split.replaceMatchedAtZero = function() {
   });
 }
 
-// The checkForLoss function loops through all the divs in the play area and checks if there are any clickable ones. If not, it gives a Game Over message.
+// The checkForLoss function looks at the board every turn to see if there are any available moves. If the boxes are all matched (matched boxes can't be clicked), or if the boxes are all too thin to be clicked, the screen will display a game over message.
 
 split.checkForLoss = function() {
   var divs = [];
@@ -232,6 +239,8 @@ split.checkForLoss = function() {
   }
 }
 
+// The restartGame function is the event listener on the restart button which is displayed on the game over screen. As well as resetting the game board, the function also sets the player score and turn count to zero, and sets the split direction to horizontal.
+
 split.restartGame = function() {
   $("#gameOver").replaceWith("<div class='b08x16'></div>");
   split.direction = "horiz";
@@ -239,7 +248,7 @@ split.restartGame = function() {
   split.score = 0;
 }
 
-// The displayTurn function pushes the turn count onto the scoreboard. The displayDirection function pushes the direction of the next split onto the scoreboard.
+// The displayTurn, displayDirection and displayScore functions are responsible for putting their respective variables onto the screen. They run after every click to ensure the correct values can be seen.
 
 split.displayTurn = function() {
   $("#turns").html(split.turn);
@@ -257,14 +266,14 @@ split.displayScore = function() {
   $("#score").html(split.score);
 }
 
-// The countUp function counts what turn you are on, adding one to the counter for every click.
+// The countUp function runs every turn to increase both the turn count and the player score by 1 (the player scores one point for every click made).
 
 split.countUp = function() {
   split.turn++;
   split.score++;
 }
 
-// The determineDirection function switches between horizontal and vertical splits, alternating every turn.
+// The determineDirection function alternates the direction of the split between horizontal and vertical every turn. On every even turn the split will be horizontal, and on every vertical turn the split will be vertical.
 
 split.determineDirection = function() {
   if (split.turn % 2 === 0) {
@@ -273,6 +282,8 @@ split.determineDirection = function() {
     split.direction = "vert";
   }
 }
+
+// The colourListeners turn sets up the buttons which change the colour of the game board.
 
 split.colourListeners = function() {
   $("#white").on("click", function() {
@@ -318,6 +329,8 @@ split.colourListeners = function() {
     $("#acknowledgement").css("border-color", "orange");
   });
 }
+
+// The playSounds function will play a sound whenever a split is made. The sound made is different for a vertical and horizontal split.
 
 split.playSounds = function() {
   var audio = $("audio")
